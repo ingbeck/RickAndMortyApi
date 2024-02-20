4d3f89a763becfdfc9263ecd827ef5b868b63dad
ingobecker <mail@ingobecker.de>
2024-02-20 11:17:39 +0100

    setup environment for MockWebServerTest

--- src/test/java/com/github/ingbeck/rickandmortyapi/service/RickAndMortyServiceTest.java
+++ src/test/java/com/github/ingbeck/rickandmortyapi/service/RickAndMortyServiceTest.java
@@ -1,7 +1,42 @@
 package com.github.ingbeck.rickandmortyapi.service;
 
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class RickAndMortyServiceTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    private static MockWebServer mockWebServer;
+
+    @BeforeAll
+    static void setupMockWebServer() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @AfterAll
+    static void shutdownMockWebServer() throws IOException{
+        mockWebServer.shutdown();
+    }
+
+    @DynamicPropertySource
+    static void backendProps(DynamicPropertyRegistry registry){
+        registry.add("basic.url", () -> mockWebServer.url("/").toString());
+    }
 
 }
